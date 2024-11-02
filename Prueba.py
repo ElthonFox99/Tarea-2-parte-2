25c205aff9283422543989146ae6209496cab5e0
Emily Rey Cárdenas <reyemily2007@gmail.com>
2024-11-02 00:01:28 -0600

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,23 @@
+print("digite sus notas")
+nota1 = int(input("Digite su primera nota: "))
+nota2 = int(input("Digite su primera nota: "))
+nota3 = int(input("Digite su primera nota: "))
+if(nota1 ==70):
+    print("Debes hacer ampliacion debido a tu primer nota")
+elif(nota1 <=60):
+    print("Reprovastes el curso debido a tu primer nota")
+else:
+    print("Lograstes pasar el curso con tu primera nota")
+if(nota2 ==70):
+    print("Debes hacer ampliacion debido a tu segunda nota")
+elif(nota2 <=60):
+    print("Reprovastes el curso debido a tu segunda nota")
+else:
+    print("Lograstes pasar el curso con tu segunda nota")
+
+if(nota3 ==70):
+    print("Debes hacer ampliacion debido a tu tercer nota")
+elif(nota3 <=60):
+    print("Reprovastes el curso debido a tu tercer nota")
+else:
+    print("Lograstes pasar el curso con tu tercer nota")
